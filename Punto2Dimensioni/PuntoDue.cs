6b476563c28ee51376cfc547a50f52a0ac2644f5
lucrezia sabba <lucrezia.sabba@icloud.com>
2020-11-19 12:56:08 +0100

    Aggiungere i file di progetto.

---
+++ Punto2Dimensioni/PuntoDue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2Dimensioni
+{
+    public class PuntoDue
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public PuntoDue(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Punto3Dimensioni/PuntoTre.cs
@@ -0,0 +1,19 @@
+﻿using Punto2Dimensioni;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto3Dimensioni
+{
+    public class PuntoTre : PuntoDue
+    {
+        public double Z { get; set; }
+
+        public PuntoTre(double x, double y, double z) : base(x, y)
+        {
+            Z = z;
+        }
+    }
+}
